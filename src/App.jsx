bb95399e30ba211492a073384501f2900827a7ea
deriansalazar <derian99.ds@gmail.com>
2024-11-25 20:45:27 +0100

    Tog bort getRandomColor och la in det i en egen fil i components mappen. Ändrade namn på nextDimension -> nextGridSize i usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
 import { useState } from 'react'
 import { SquareGrid } from './components/SquareGrid'
+import { getRandomColor } from './components/RandomColor';
 import './App.css'
 import "typeface-roboto";
 
-
-function getRandomColor(excludeColors) {
-  const letters = '0123456789ABCDEF'
-  let color;
-
-  const excludeSet = new Set(excludeColors); //konvertera till set
-
-  do {
-    color = "#";
-    for (let i = 0; i < 6; i++) {
-      color += letters [Math.floor(Math.random() * 16)];
-    }
-  } while (excludeSet.has(color)); //Använd 'Set' för att kontrollera färg
-
-  return color;
-}
-
-
 function App() {
   const [squares, setSquares] = useState([])
-  const [nextDimension, setNextDimension] = useState(1)
+  const [nextGridsize, setNextGridSize] = useState(1)
 
   function addSquare() {
     const excludeColors = squares.length > 0 ? squares[squares.length - 1].color : null;
@@ -31,22 +14,22 @@ function App() {
       id: squares.length + 1,
       color: getRandomColor(excludeColors)
     };
+
     const newSquares = Array.isArray(squares) ? [...squares, newSquare] : [newSquare];
 
-    if (newSquares.length > nextDimension * nextDimension) {
-      setNextDimension(nextDimension + 1);
+    if (newSquares.length > nextGridsize * nextGridsize) {
+      setNextGridSize(nextGridsize + 1);
     }
     setSquares(newSquares)
   }
 
   function clearSquares() {
     setSquares([]);
-    setNextDimension(1);
+    setNextGridSize(1);
   }
 
   return (
     <>
-
     <header className='logo'>
       <img src="/src/img/wizardworks-logo-white.svg" alt="" />
     </header>
@@ -55,16 +38,13 @@ function App() {
       
       <div className='button-container'>
         <button onClick={addSquare}>Add square</button>
-        <button onClick={clearSquares}>Clear</button>
+        <button onClick={clearSquares} style={{marginLeft: '10px', backgroundColor: '#F77171', width: '100px'}}>Clear</button>
       </div>
 
       <div className='square-container'>
-        <SquareGrid squares={squares} dimension={nextDimension} />
+        <SquareGrid squares={squares} dimension={nextGridsize} />
       </div>
-
-
     </div>
-   
     </>
   )
 }
